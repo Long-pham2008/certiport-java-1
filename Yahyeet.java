ffea52fb8c0bc0b974c2e596875a6210fbf97b5c
Long-pham2008 <phamhoanglong180108@gmail.com>
2026-05-20 03:05:21 +0000

    hola

--- Yahyeet.java
+++ Yahyeet.java
@@ -13,8 +13,29 @@ public class Yahyeet
         {
             System.out.println("You entered\nargs[0]: " + args[0] + "\nargs[1]: " + args[1]);
 
-
             // TO DO: Calculate student average scores here
+            Scanner input = new Scanner(new File(args[0]));
+            int group = Integer.parseInt(args[1]);
+
+            int count = 0;
+            double sum = 0.0;
+            while (input.hasNextInt()) // still has next term
+            {
+                int score = input.nextInt();
+                sum = sum + score;
+                count++;
+            }
+
+            if (count > 0)
+            {
+                double average = sum / count;
+                System.out.println("Average score is: " + average);
+            }
+
+            else
+            {
+                System.out.println("Not found");
+            }
         }
         else
         {
